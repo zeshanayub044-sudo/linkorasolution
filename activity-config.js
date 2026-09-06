bd5eb76a229093a0bb0a0931f3533ac760e04e63
zeshanayub044-sudo <zeshanayub044@gmail.com>
2026-09-05 20:58:06 -0700

    Add files via upload

--- activity-config.js
+++ activity-config.js
@@ -1,4 +1,4 @@
 window.LINKORA_GOOGLE_ACTIVITY_CONFIG = {
-  apiUrl: "",
-  googleClientId: ""
+  apiUrl: "https://script.google.com/macros/s/AKfycbwW6VsO7X2Zcf0gDTXusK7EE4JdcdN2aCMZjI5BgKd6IdSoQxWrE_yy-ud_YOSrK5fT/exec",
+  googleClientId: "424514060121-u6q7hu76jlotts34kej6qnqvbd18cver.apps.googleusercontent.com"
 };
